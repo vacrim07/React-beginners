47d1de9113f7cbb575899fc046b93aad761c4899
Vladimir Vasquez <vacrim07@gmail.com>
2020-05-02 16:51:31 -0500

    Login with firebase

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import firebase from "firebase";
 import AddFishForm from './AddFishForm';
 import EditFishForm from "./EditFishForm";
+import Login from "./Login"
+import {firebaseApp} from "../base"
 
 class Inventory extends React.Component {
 
@@ -12,7 +15,17 @@ class Inventory extends React.Component {
 		loadSampleFishes: PropTypes.func
 	}
 
+	authHandler = async authData => {
+		console.log(authData);
+	}
+
+	authenticate = provider => {
+		const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+		firebaseApp.auth().signInWithPopup(authProvider).then(this.autHandler);
+	};
+
 	render() {
+		return <Login authenticate={this.authenticate} />;
 		return <div className="inventory">
 			<h2>Inventory</h2>
 			{Object.keys(this.props.fishes)
